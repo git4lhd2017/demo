4d9fcf693facdf51f51c71fdad07f75f3299c5fc
lingfeng2016 <sxy7954@163.com>
2019-12-29 19:29:15 +0800

    新建一个类

--- src/main/java/demo/Two.java
+++ src/main/java/demo/Two.java
@@ -1,26 +1,20 @@
 package demo;
 
 /**
- * All rights Reserved, Designed By www.info4z.club
+ * All rights Reserved, Designed By 网址
  * <p>title:demo</p>
- * <p>ClassName:One</p>
+ * <p>ClassName:Two</p>
  * <p>Description:TODO(请用一句话描述这个类的作用)</p>
  * <p>Compony:Info4z</p>
  * author:poker_heart
- * date:2019/12/17
+ * date:2019/12/29
  * version:1.0
  * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
  */
-public class One {
-
+public class Two {
 
     public static void main(String[] args) {
-        Integer a = (Integer) 129;
-        Integer b = (Integer) 129;
-        System.out.println(a==b);
+        System.out.println("hello word !!!");
     }
-
-
-
 
 }

3978e9090c13c00851924906a91e404bc6103e1f
git4lhd2017 <number4l@163.com>
2019-12-29 19:53:11 +0800

    新建了一个类

---
+++ src/main/java/lhd/LHD.java
@@ -0,0 +1,20 @@
+package lhd;
+
+/**
+ * All rights Reserved, Designed By www.info4z.club
+ * <p>title:lhd</p>
+ * <p>ClassName:LHD</p>
+ * <p>Description:TODO(请用一句话描述这个类的作用)</p>
+ * <p>Compony:Info4z</p>
+ * author:poker_heart
+ * date:2019/12/29
+ * version:1.0
+ * 注意：本内容仅限于公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+public class LHD {
+
+    public static void main(String[] args) {
+        System.out.println("我是LHD！！");
+    }
+
+}
